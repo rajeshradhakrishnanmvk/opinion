dae0a3624df7f929cd401b53f178c2b100bbb266
rajeshmvk <rajeshmvk@gmail.com>
2025-10-01 15:57:40 +0000

    DB Presistance Issue

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,12 @@ import {initializeApp, getApp, getApps} from 'firebase/app';
 import {getDatabase} from 'firebase/database';
 
 const firebaseConfig = {
-  projectId: 'studio-5115947685-97557',
-  appId: '1:804290117937:web:d61a0bdecccea26a4c3010',
-  apiKey: 'AIzaSyCVMlDLH-bXFIuUlLdzItZ2BEbIupEgm-o',
-  authDomain: 'studio-5115947685-97557.firebaseapp.com',
-  measurementId: '',
-  messagingSenderId: '804290117937',
-  // By removing the databaseURL, the SDK will automatically use the default database.
+  apiKey: "AIzaSyCk7_1jtouKGdp0CH1-9u8povL3OBVr-KA",
+  authDomain: "opinion-8675.firebaseapp.com",
+  projectId: "opinion-8675",
+  storageBucket: "opinion-8675.firebasestorage.app",
+  messagingSenderId: "480541249077",
+  appId: "1:480541249077:web:aba10977641fc0c7b1eb7c"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
